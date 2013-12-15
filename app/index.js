162af43264ad6035c3e7933fbf11d49f3d4465f8
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2013-12-15 05:23:14 +0100

    Initial makup

--- app/index.js
+++ app/index.js
@@ -21,16 +21,90 @@ WptGenerator.prototype.askFor = function askFor() {
 
   // have Yeoman greet the user.
   console.log(this.yeoman);
+  console.log(this.pkg.name + ' generator');
+  console.log('Out of the box I include HTML5 Boilerplate and jQuery.');
 
   var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+    type: 'list',
+    name: 'preproCss',
+    message: 'Do you prefer sass, less, or just css?',
+    choices: [{
+      name: 'Sass',
+      value: 'sass',
+    }, {
+      name: 'Less',
+      value: 'less',
+    }, {
+      name: 'CSS',
+      value: 'css',
+    }],
+    default: 0
+  },
+  {
+    type: 'checkbox',
+    name: 'useCss',
+    message: 'Any CSS helper?',
+    choices: [{
+      name: 'Bootstrap',
+      value: 'useBootstrap',
+      checked: true
+    }, {
+      name: 'Autoprefixer',
+      value: 'useAutoprefixer',
+      checked: true
+    }, {
+      name: 'Coffeescript',
+      value: 'useCoffee',
+      checked: false
+    }, {
+      name: 'Unit-testing',
+      value: 'useTest',
+      checked: false
+    }]
+  },
+  {
+    type: 'checkbox',
+    name: 'useJs',
+    message: 'And what about javascript?',
+    choices: [{
+      name: 'Jshint',
+      value: 'useJshint',
+      checked: false
+    }, {
+      name: 'Modernizr',
+      value: 'useModernizr',
+      checked: false
+    }, {
+      name: 'RequireJS',
+      value: 'useRequirejs',
+      checked: false
+    }, {
+      name: 'Coffeescript',
+      value: 'useCoffee',
+      checked: false
+    }, {
+      name: 'Unit-testing',
+      value: 'useTest',
+      checked: false
+    }]
   }];
 
   this.prompt(prompts, function (props) {
     this.someOption = props.someOption;
+
+    // Ugly default config overide
+    this.starterTheme =     'roots';
+    this.cssFramework =     'bootstrap';
+    this.testFramework =    'mocha';
+
+    this.useJshint =        props.useJs['useJshint'];
+    this.useModernizr =     false;
+    this.useCoffee =        false;
+    this.useTest =          false;
+    this.useRequirejs =     false;
+    this.preproCss =        'less';
+    this.useAutoprefixer =  false;
+    this.useImagemin =      false;
 
     cb();
   }.bind(this));
