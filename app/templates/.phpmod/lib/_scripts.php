ff4423d0e046dffd24d1b80b8d8a9962d788aca8
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2013-12-16 04:58:18 +0100

    switched from main.min.css to main.css and changed app.{less,scss} to main

--- app/templates/.phpmod/lib/_scripts.php
+++ app/templates/.phpmod/lib/_scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '75f647ac6f039169167b74aea9f6012d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.css', false, '75f647ac6f039169167b74aea9f6012d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
